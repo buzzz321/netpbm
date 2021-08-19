ff0165c0ad6c4071894368e82216cb56145348ee
Anders Olme <anders.olme@gmail.com>
2021-08-19 19:37:14 +0200

    Now we can fill the buffer!!

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,29 @@ use std::io::prelude::*;
 
 pub(crate) const WIDTH: u16 = 640;
 pub(crate) const HEIGHT: u16 = 480;
+const BUFF_SIZE: u32 = (WIDTH as u32 * HEIGHT as u32 * 3) as u32;
 
-fn display_buffer(file: &mut File) -> std::io::Result<()> {
+fn fill_buffer(buffer: &mut [u8]) {
+    for elem in buffer {
+        *elem = 128;
+    }
+}
+
+fn display_buffer(file: &mut File, buffer: &[u8; BUFF_SIZE as usize]) -> std::io::Result<()> {
     let header = format!("P6\n{} {}\n255\n", WIDTH, HEIGHT);
 
     file.write(header.as_bytes())?;
+    file.write(buffer)?;
 
-    let data: [u8; 3] = [255, 0, 250];
-    for _i in 0..WIDTH {
-        for _n in 0..HEIGHT {
-            file.write(&data)?;
-        }
-    }
     Ok(())
 }
 fn main() -> std::io::Result<()> {
     {
         let mut file = File::create("test.ppm")?;
+        let mut buffer: [u8; BUFF_SIZE as usize] = [0; BUFF_SIZE as usize];
         // Write a slice of bytes to the file
-        display_buffer(&mut file)?;
+        fill_buffer(&mut buffer);
+        display_buffer(&mut file, &buffer)?;
     }
 
     Ok(())
