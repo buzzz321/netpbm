3eea04823da3479c9ed13e391988dc79cbc97ba3
Anders Olme <anders.olme@gmail.com>
2021-08-20 18:06:17 +0200

    Now we can plot pixes with sizes.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,17 @@ pub(crate) const WIDTH: u32 = 640;
 pub(crate) const HEIGHT: u32 = 480;
 const BUFF_SIZE: u32 = (WIDTH as u32 * HEIGHT as u32 * 3) as u32;
 
-fn plot_pixel(buffer: &mut [u8], x: u32, y: u32, colour: u32) {
-    let index = x * 3 + y * WIDTH * 3;
-    println!("{:?}", index);
-    buffer[index as usize] = ((colour >> 16) & 0xff) as u8; //r
-    buffer[(index + 1) as usize] = ((colour >> 8) & 0xff) as u8; //g
-    buffer[(index + 2) as usize] = ((colour >> 0) & 0xff) as u8; //b
+fn plot_pixel(buffer: &mut [u8], x: u32, y: u32, colour: u32, size: u32) {
+    for numpixy in 0..size {
+        for numpix in 0..size {
+            let index = (x + numpix).clamp(0, WIDTH - 1) * 3
+                + (y + numpixy).clamp(0, HEIGHT - 1) * WIDTH * 3;
+            println!("{:?}", index);
+            buffer[index as usize] = ((colour >> 16) & 0xff) as u8; //r
+            buffer[(index + 1) as usize] = ((colour >> 8) & 0xff) as u8; //g
+            buffer[(index + 2) as usize] = ((colour >> 0) & 0xff) as u8; //b
+        }
+    }
 }
 
 fn fill_buffer(buffer: &mut [u8], colour: u32) {
@@ -40,7 +45,7 @@ fn main() -> std::io::Result<()> {
         let mut buffer: [u8; BUFF_SIZE as usize] = [0; BUFF_SIZE as usize];
         // Write a slice of bytes to the file
         fill_buffer(&mut buffer, 0xffffff);
-        plot_pixel(&mut buffer, 320, 240, 0xff00FF);
+        plot_pixel(&mut buffer, 320, 240, 0xff00FF, 8);
         display_buffer(&mut file, &buffer)?;
     }
 
